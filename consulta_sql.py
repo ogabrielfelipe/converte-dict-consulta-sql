01adda319d20b0d01a9a8e0a45f51b01de37e2ff
Gabriel <gabrielfelipe100x@gmail.com>
2022-03-05 13:11:25 -0300

    ajustes

--- consulta_sql.py
+++ consulta_sql.py
@@ -3,7 +3,7 @@ def convert_pesquisa_consulta(dict):
     for chave, valor in dict.items():
         atributo = chave
         operador = ''
-        
+
         for i in range(len(valor)):
 
             if valor[i] == 'LIKE':
@@ -57,16 +57,3 @@ def convert_pesquisa_consulta(dict):
                         cons_sql.append("{}  {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+1]))
     
     return " ".join(cons_sql)[4:]
-    
-
-
-entry2 = {
-    "nome": ['AND', 'LIKE', '%_%', 'Gabriel'],
-    "data": ['OR', 'BETWEEN', ['2022-03-01', '2022-03-31']],
-    "id": ['OR', '=', 1],
-    "status": ['OR', '!=', 1],
-}    
-
-resultado2 = convert_pesquisa_consulta(entry2)
-print(resultado2)
-
