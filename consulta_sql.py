1c0676560577f21537538ad8f3be3cd15492d330
Gabriel <gabrielfelipe100x@gmail.com>
2022-03-05 13:06:59 -0300

    ajustes

--- consulta_sql.py
+++ consulta_sql.py
@@ -3,36 +3,58 @@ def convert_pesquisa_consulta(dict):
     for chave, valor in dict.items():
         atributo = chave
         operador = ''
+        
         for i in range(len(valor)):
-            if valor[i] == 'OR':
-                operador_sql_or = valor[i]
-                if valor[i+1] == 'LIKE':
-                    operador = valor[i+1]
-                    cons_sql.append("{}  {} {} '{}'".format(operador_sql_or, atributo, operador, valor[i+2].replace('_', valor[i+3])))
-                if valor[i+1] == '=':                
-                    operador = valor[i+1]
-                    cons_sql.append('{}  {} {} {}'.format(operador_sql_or, atributo, operador, valor[i+2]))
-                if valor[i+1] == '!=':                
-                    operador = valor[i+1]
-                    cons_sql.append('{} {} {} {}'.format(operador_sql_or, atributo, operador, valor[i+2]))
-                if valor[i+1] == 'BETWEEN':            
-                    operador = valor[i+1]
-                    cons_sql.append("{}  {} {} '{}' AND '{}'".format(operador_sql_or, atributo, operador, valor[i+2][0], valor[i+2][1]))
 
-            elif valor[i] == 'AND':
-                operador_sql_and = valor[i]
-                if valor[i+1] == 'LIKE':
-                    operador = valor[i+1]
-                    cons_sql.append("{} {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+2].replace('_', valor[i+3])))
-                if valor[i+1] == '=':                
-                    operador = valor[i+1]
-                    cons_sql.append('{} {} {} {}'.format(operador_sql_and, atributo, operador, valor[i+2]))
-                if valor[i+1] == '!=':                
-                    operador = valor[i+1]
-                    cons_sql.append('{} {} {} {}'.format(operador_sql_and, atributo, operador, valor[i+2]))
-                if valor[i+1] == 'BETWEEN':            
-                    operador = valor[i+1]
-                    cons_sql.append("{} {} {} '{}' AND '{}'".format(operador_sql_and, atributo, operador, valor[i+2][0], valor[i+2][1]))
+            if valor[i] == 'LIKE':
+                operador = valor[i-1]
+                if valor[i-1] == 'OR':
+                    operador_sql_or = valor[i]
+                    cons_sql.append("{}  {} {} '{}'".format(operador_sql_or, atributo, operador, valor[i+1].replace('_', valor[i+2])))
+                if valor[i-1] == 'AND':
+                    operador_sql_and = valor[i]
+                    cons_sql.append("{} {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+1].replace('_', valor[i+2])))
+            
+            elif valor[i] == 'BETWEEN':
+                operador = valor[i]
+                if valor[i-1] == 'OR':
+                    operador_sql_or = valor[i-1]                    
+                    cons_sql.append("{}  {} {} '{}' AND '{}'".format(operador_sql_or, atributo, operador, valor[i+1][0], valor[i+1][1]))
+                if valor[i-1] == 'AND':
+                    operador_sql_and = valor[i-1]  
+                    cons_sql.append("{} {} {} '{}' AND '{}'".format(operador_sql_and, atributo, operador, valor[i+1][0], valor[i+1][1]))
+
+            elif valor[i] == '=':
+                operador = valor[i]
+                if valor[i-1] == 'OR':
+                    operador_sql_and = valor[i-1]
+                    if isinstance(valor[i+1], float) or isinstance(valor[i+1], int):
+                        cons_sql.append("{}  {} {} {}".format(operador_sql_and, atributo, operador, valor[i+1]))
+                    else:
+                        cons_sql.append("{}  {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+1])) 
+                        
+                operador_sql_and = valor[i-1] 
+                if valor[i-1] == 'AND':
+                    if isinstance(valor[i+1], float) or isinstance(valor[i+1], int):
+                        cons_sql.append("{} {} {} {}".format(operador_sql_and, atributo, operador, valor[i+1]))
+                    else:
+                        cons_sql.append("{} {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+1]))
+
+            elif valor[i] == '!=':
+                operador = valor[i]
+                if valor[i-1] == 'OR':
+                    operador_sql_and = valor[i-1]
+                    if isinstance(valor[i+1], float) or isinstance(valor[i+1], int):
+                        cons_sql.append("{}  {} {} {}".format(operador_sql_and, atributo, operador, valor[i+1]))
+                    else:
+                        cons_sql.append("{}  {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+1]))
+
+                operador_sql_and = valor[i-1]
+                if valor[i-1] == 'AND':
+                    if isinstance(valor[i+1], float) or isinstance(valor[i+1], int):
+                        cons_sql.append("{} {} {} {}".format(operador_sql_and, atributo, operador, valor[i+1]))
+                    else:
+                        cons_sql.append("{}  {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+1]))
     
     return " ".join(cons_sql)[4:]
     
@@ -41,7 +63,7 @@ def convert_pesquisa_consulta(dict):
 entry2 = {
     "nome": ['AND', 'LIKE', '%_%', 'Gabriel'],
     "data": ['OR', 'BETWEEN', ['2022-03-01', '2022-03-31']],
-    "id": ['AND', '=', 1],
+    "id": ['OR', '=', 1],
     "status": ['OR', '!=', 1],
 }    
 
